d1cef8f1682636e0ad79f71fbd765a68516fd301
OlBul <olhabulhakova@gmail.com>
2022-12-11 20:58:17 +0200

    add class

--- src/pages/ContactsUs/ContactsUs.tsx
+++ src/pages/ContactsUs/ContactsUs.tsx
@@ -74,12 +74,13 @@ const ContactsUs = (props: Props) => {
                                 </a>
                             </div>
                         </div>
-                        <div className="contact__map"></div>
-                        <iframe
-                            className="map"
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d164153.23443639991!2d36.2858247!3d49.99459135!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4127a1000e0da5b9%3A0x7bc3ddab9336dd55!2sMaxim%20Gorky%20Central%20Park%20for%20Culture%20and%20Recreation!5e0!3m2!1sen!2spl!4v1663790270695!5m2!1sen!2spl"
-                            title="address"
-                        ></iframe>
+                        <div className="contact__map">
+                            <iframe
+                                className="map"
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d164153.23443639991!2d36.2858247!3d49.99459135!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4127a1000e0da5b9%3A0x7bc3ddab9336dd55!2sMaxim%20Gorky%20Central%20Park%20for%20Culture%20and%20Recreation!5e0!3m2!1sen!2spl!4v1663790270695!5m2!1sen!2spl"
+                                title="address"
+                            ></iframe>
+                        </div>
                     </div>
                 </div>
             </section>
